f4044c5d8624ee57902bdb9f5a73e07985587779
Emmanuel Okedele <freelanceokeks@gmail.com>
2023-05-08 16:34:02 +0000

    08/05

---
+++ deploy/auth.sh
@@ -0,0 +1,6 @@
+export OPENAI_API_KEY="K5ZePj3GkfHdYDRG"
+export MODEL_NAME="text-davinci-003"
+export CHAT_MODEL="gpt-3.5-turbo"
+export API_SECRET="ePasLO1vmptWmhHPhtFEEhQ86qnhJcsP"
+
+echo Done
---
+++ todo.md
@@ -0,0 +1,1 @@
+#CI actions for docker build
